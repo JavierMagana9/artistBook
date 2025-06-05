fda03ed0a98e2d632046560562cb5a27250debfa
JavierMagana9 <javier.magana9@gmail.com>
2025-06-05 13:10:02 +0100

    details

--- front/src/utils/firebase.js
+++ front/src/utils/firebase.js
@@ -1,4 +1,4 @@
-Users/javi/Desktop/artistBook/front/src/utils/firebase.js
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
